4b6bc4c0c50283bcee5c14a98e234f4f5eec2ea3
Eliza Dunaway <email": null>
2015-12-05 17:46:28 -0500

    adds round controller

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -4,3 +4,11 @@ GET '/rounds/:id' do
   @deck = Deck.find(@round.deck_id)
   erb :'rounds/show'
 end
+
+get '/deck/:id/rounds/' do
+  @deck = Deck.find_by(id: params[:id])
+  @round = Round.create(deck_id: @deck.id, user_id: current_user.id, first_guesses: 0, total_guesses: 0)
+  num_of_cards = @deck.cards.length
+  random_card_id = [1..num_of_cards].sample
+  redirect "/rounds/#{@round.id}/cards/#{random_card_id}"
+end
