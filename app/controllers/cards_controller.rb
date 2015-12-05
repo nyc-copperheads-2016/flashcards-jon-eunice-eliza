fff5bee3d581d33590f74e5392ae993208668a00
Eliza Dunaway <email": null>
2015-12-05 17:59:49 -0500

    add jons correction for route in cards_controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,4 @@
-get '/rounds/:round.id/cards/:id' do
+get '/rounds/:round_id/cards/:id' do
   @deck = Deck.find_by(id: params[:deck_id])
   @card = @deck.cards.find_by(id: params[:id])
   erb :'cards/show'
