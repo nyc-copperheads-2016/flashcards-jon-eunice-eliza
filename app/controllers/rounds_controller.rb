46ba1d27563b83f83c3eea3f94204266e97c05b2
dijonkitchen <jschen2008@gmail.com>
2015-12-04 21:30:47 -0500

    Add iteration through decks and rounds

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,9 +1,12 @@
 GET '/rounds' do
+  @rounds = Rounds.all
+  @decks = Decks.all
   erb :'rounds/index'
 end
 
 GET '/rounds/new' do
-
+  @deck = Deck.find(params[:deck_id])
+  @round = Round.create(deck_id: params[:deck_id], user_id: params[:user_id])
 end
 
 POST '/rounds' do
@@ -11,13 +14,5 @@ POST '/rounds' do
 end
 
 GET '/rounds/:id' do
-
-end
-
-PUT '/rounds' do
-
-end
-
-DELETE '/rounds' do
 
 end
