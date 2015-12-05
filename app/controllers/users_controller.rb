04f3a04fb8450fe4db4ce2b7e2bd89990f9a9111
Jeff George <webdevjeffus@gmail.com>
2015-12-05 17:51:05 -0500

    Implement /user/show viewer and /user/:id route.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+get '/users/new' do
+
+end
+
+
+get '/users/:id' do
+  rounds = current_user.rounds.all
+  decks_played = {}
+  rounds.each do |round|
+    # Deck is already in the hash
+    if decks_played.has_key?(round.id)
+      decks_played[round.id] << round
+    # Deck is not in the hash
+    else
+      decks_played[round.id] = []
+      decks_played[round.id] << round
+    end
+
+  erb :"/users/show", locals: { decks_played: decks_played }
+end
