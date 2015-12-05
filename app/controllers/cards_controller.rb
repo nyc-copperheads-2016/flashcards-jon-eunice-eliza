c34e138e09fbcd200b1e46e4c83984207ae2a923
Eliza Dunaway <email": null>
2015-12-05 18:26:49 -0500

    add until loop that runs game in cards_controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,10 @@
 get '/rounds/:round_id/cards/:id' do
   @deck = Deck.find_by(id: params[:deck_id])
   @card = @deck.cards.find_by(id: params[:id])
-  erb :'cards/show'
+  until @cards.length == 0
+    @card = @cards.shift
+    erb :'cards/show'
+  else
+    redirect "/rounds/#{params[:round_id]}"
+  end
 end
