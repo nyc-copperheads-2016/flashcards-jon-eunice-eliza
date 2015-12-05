acb17db2b669149596ad67effc2da3e59e9ee3d7
dijonkitchen <jschen2008@gmail.com>
2015-12-04 20:51:28 -0500

    Add REST routes skeleton

---
+++ app/controllers/rounds_controller.rb
@@ -0,0 +1,23 @@
+GET '/rounds' do
+  erb :'rounds/index'
+end
+
+GET '/rounds/new' do
+
+end
+
+POST '/rounds' do
+
+end
+
+GET '/rounds/:id' do
+
+end
+
+PUT '/rounds' do
+
+end
+
+DELETE '/rounds' do
+
+end
