8ef399485ab8b1c445787bb22aaf802689c9aff0
Eunice Do <email": null>
2015-12-05 20:16:09 -0500

    save user auth page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,24 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  include BCrypt
 
   has_many :guesses
   has_many :rounds
   has_many :decks, through: :rounds
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true
+
+  attr_accessor :password_confirmation
+
+  def password
+    @password ||= Password.new(password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password = @password
+  end
 end
