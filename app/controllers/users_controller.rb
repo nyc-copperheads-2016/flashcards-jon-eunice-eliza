0d791ef41c5467e445cddd6dafa288502fd153ce
Eliza Dunaway <email": null>
2015-12-06 22:38:09 -0500

    Fix profile page for deck sorting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,18 +12,18 @@ post '/users' do
 end
 
 get '/users/:id' do
-  rounds = current_user.rounds.all
+  rounds = current_user.rounds.all.order(:deck_id)
   decks_played = {}
   rounds.each do |round|
     # Deck is already in the hash
-    if decks_played.has_key?(round.id)
-      decks_played[round.id] << round
+    if decks_played.has_key?(round.deck_id)
+      decks_played[round.deck_id] << round
     # Deck is not in the hash
     else
-      decks_played[round.id] = []
-      decks_played[round.id] << round
+      decks_played[round.deck_id] = []
+      decks_played[round.deck_id] << round
     end
   end
-
+  # binding.pry
   erb :"users/show", locals: { decks_played: decks_played }
 end
