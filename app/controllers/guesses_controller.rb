1a8a1c10e4ff92df734a60f59fe00f92678ad371
Eliza Dunaway <email": null>
2015-12-06 22:07:34 -0500

    Add first guesses count

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -7,6 +7,14 @@ post '/cards/:id/guesses' do
 
   if params[:answer].downcase == @card.answer.downcase
     guess = @round.guesses.find_by(card_id: @card.id)
+    binding.pry
+    count = @round.deck.cards.find(@card.id).guesses.where(round_id: @round.id).count
+
+    if count == 1
+      guess_count = @round.first_guesses + 1
+      @round.update_attributes(first_guesses: guess_count)
+    end
+
     guess.update_attributes(solved: true)
     remaining_cards = Guess.where(["solved = ? and round_id = ?", "false", @round.id ]).order(:id)
     # binding.pry
