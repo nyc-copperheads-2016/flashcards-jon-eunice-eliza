a2304af0a0b3221745077e827d6d34ffce98c1de
Eliza Dunaway <email": null>
2015-12-06 22:48:19 -0500

    Fix seed data and code jokes name quotes

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -3,16 +3,21 @@ post '/cards/:id/guesses' do
   @round = Round.find(session[:current_round_id])
   @round.total_guesses += 1
 
-  if Guess.where(["solved = ? and round_id = ?", "false", @round.id ])
-    erb :'rounds/show'
-  end
 
   if params[:answer].downcase == @card.answer.downcase
     guess = @round.guesses.find_by(card_id: @card.id)
     guess.update_attributes(solved: true)
     remaining_cards = Guess.where(["solved = ? and round_id = ?", "false", @round.id ]).order(:id)
-    @card = remaining_cards.first.card
-    erb :'/cards/show'
+    # binding.pry
+    if remaining_cards.length == 0
+      redirect '/rounds/#{@round.id}'
+    else
+      # binding.pry
+
+      @card = remaining_cards.first.card
+      erb :'/cards/show'
+    end
+
   else
     @last_card_was_wrong_answer = @card.answer
     erb :'/cards/show'
--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -7,9 +7,8 @@ end
 
 get '/decks/:id/rounds/new' do
   @deck = Deck.find_by(id: params[:id])
-  # user_id = 1 until we have a session or user stuff
-  @round = Round.create(deck_id: @deck.id, user_id: 1, first_guesses: 0, total_guesses: 0)
-  session[:current_round_id] = @rounds.id
+  @round = Round.create(deck_id: @deck.id, user_id: current_user.id, first_guesses: 0, total_guesses: 0)
+  session[:current_round_id] = @round.id
   @deck.cards.each do |card|
     Guess.create(card_id: card.id, round_id: @round.id, user_id: 1)
   end
